eb0cf3c7dfcae6fe750bf943a727005a6eb4e2e3
mahablaa <hsy2000l@ewhain.net>
2024-08-07 14:50:40 +0900

    Title: 알파벳에 따른 평가, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240807/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+eng = input()
+
+if eng == "S":
+    print("Superior")
+elif eng == "A":
+    print("Excellent")
+elif eng == "B":
+    print("Good")
+elif eng == "C":
+    print ("Usually")
+elif eng == "D":
+    print("Effort")
+else:
+    print("Failure")
