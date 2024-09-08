35b4d9b7e7c2fc82c5360b7e1150a3dcd8d41129
mahablaa <hsy2000l@ewhain.net>
2024-09-08 23:10:52 +0900

    Title: 100점 도달하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240908/100점 도달하기/reach-100-point.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for i in range (n, 101):
+    if i < 60:
+        print("F", end = " ")
+    elif i < 70:
+        print("D", end = " ")
+    elif i < 80:
+        print("C", end = " ")
+    elif i < 90:
+        print("B", end = " ")
+    else:
+        print("A", end = " ")
