bdad964a0220dfc9a03443a7197d996d1d78c0a3
mahablaa <hsy2000l@ewhain.net>
2024-11-28 11:27:50 +0900

    Title: 장학금, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241128/장학금/scholarship.py
+++ 241128/장학금/scholarship.py
@@ -2,8 +2,8 @@ arr = input(). split()
 a, b = int(arr[0]), int(arr[1])
 
 if a >= 90 and b >= 95:
-    print(10)
+    print(100000)
 elif a >= 90 and b >= 90:
-    print(5)
+    print(50000)
 else:
     print(0)
