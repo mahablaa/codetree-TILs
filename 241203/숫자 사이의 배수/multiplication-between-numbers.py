1d148209fc8b3dd885b2965815b746e1d45e2978
mahablaa <hsy2000l@ewhain.net>
2024-12-03 15:18:25 +0900

    Title: 숫자 사이의 배수, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241203/숫자 사이의 배수/multiplication-between-numbers.py
@@ -0,0 +1,12 @@
+arr = input(). split()
+a, b = int(arr[0]), int(arr[1])
+
+cnt = 0
+sum_val = 0
+for i in range (a, b+1):
+    if i%5==0 or i%7==0:
+        cnt += 1
+        sum_val += i
+
+print(sum_val, end = "")
+print(f"{sum_val / cnt : .1f}")
